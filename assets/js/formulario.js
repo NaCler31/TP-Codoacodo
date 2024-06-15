5903cddb90d3322c1b20de033d1e2b63873a8627
Natalia Clerici <130027655+NaCler31@users.noreply.github.com>
2024-06-14 23:06:49 -0300

    Update formulario.js
    validar

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,34 +1,49 @@
-const formRegister=document.getElementById("formulario");
+const formularioRegister=document.getElementById("formulario")
 const inputNombre=document.getElementById("nombre");
 const inputEmail=document.getElementById("email");
-const inputMotivo=document.getElementById("motivo");
+const inputSeleccione=document.getElementById("seleccione");
 const parrafo=document.getElementById("error");
+const mensajeEnviado = document.getElementById("mensajeEnviado");
+
 
 // console.log(inputNombre);
 
-formRegister.addEventListener("submit",e=>{
+formularioRegister.addEventListener("submit",e=>{
 e.preventDefault();
 let warning="";
 let valor=false;
 parrafo.innerHTML="";
-let regexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let regexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 if(inputNombre.value.length<3){
-   warning+="Escriba un nombre<br>";
+   warning+="Escriba un nombre<br>"
    valor=true;
 }
 if(!regexEmail.test(inputEmail.value)){
-    warning+="Escriba un email válido<br>";
+    warning+="Escriba un email válido<br>"
     valor=true;
 }
-if(inputMotivo.value=="default"){
-    warning+="Elija una opcion <br>";
+if(inputSeleccione.value=="default"){
+    warning+="Elija una opcion <br>"
     valor=true;
 }
 if(valor){
     parrafo.innerHTML=warning;
 }else{
-    parrafo.innerHTML="Su solicitud fue enviada correctamente te responderemos pronto";
+    parrafo.innerHTML="Su solicitud fue enviada correctamente te responderemos pronto"
     formRegister.reset();
 }
 })
+
+if (window.alert("¡Formulario enviado con éxito!")) {
+    window.location.reload();
+}
+
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita la recarga de la página
+
+    // Aquí puedes realizar otras acciones (enviar datos al servidor, etc.)
+
+    // Muestra el mensaje de éxito
+    mensajeEnviado.style.display = "block";
+});
